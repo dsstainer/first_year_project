428da2de8b10a9c7846abdd4be061ffade10b0a5
q03860jw <jonathan.wilson-6@student.manchester.ac.uk>
2023-12-02 21:37:30 +0000

    added some ws stuff

---
+++ src/routes/websockets/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { io } from 'socket.io-client';
+
+	const socket = io();
+
+	socket.on('eventFromServer', (message) => {
+		console.log(message);
+	});
+</script>
+
+<main>
+	<h1>Hello, SvelteKit with WebSocket!</h1>
+</main>
