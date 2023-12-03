c7a9ff85771fdafc6a21a35adbdc2f492b0784da
q03860jw <jonathan.wilson-6@student.manchester.ac.uk>
2023-12-03 13:57:53 +0000

    typescript prodserver and websockets

--- websockets.ts
+++ websockets.ts
@@ -1,4 +1,6 @@
-export const setupConnections = (io) => {
+import type { Server } from 'socket.io';
+
+export const setupConnections = (io: Server) => {
   io.on('connection', (socket) => {
     socket.emit('eventFromServer', 'Hello, World 👋');
   });
