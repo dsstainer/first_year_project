357a8192e94554b3a6dcdda59e4888345b994eb1
q03860jw <jonathan.wilson-6@student.manchester.ac.uk>
2023-12-04 12:11:38 +0000

    p5 test

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,8 +4,11 @@ import type { Handle } from '@sveltejs/kit';
 import PocketBase from 'pocketbase';
 
 export const handle: Handle = async ({ event, resolve }): Promise<Response> => {
-    event.locals.pb = new PocketBase(process.env.POCKETBASE_URL || "http://localhost:8090/");
-
+    try {
+        event.locals.pb = new PocketBase(process.env.POCKETBASE_URL || "http://localhost:8090/");
+    } catch (e) {
+        console.log("can't connect to pocketbase");
+    }
     const response = await resolve(event);
     return response;
 }
