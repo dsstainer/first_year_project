24fb25da2b57d8b85cb7d71367d4024ec1827e08
Jonathan Wilson <jonathan.wilson-6@student.manchester.ac.uk>
2024-02-01 10:43:25 +0000

    basic session list

--- src/routes/sessionlist/+page.server.ts
+++ src/routes/sessionlist/+page.server.ts
@@ -1,13 +1,13 @@
 import type { PageServerLoadEvent } from "./$types";
 
 // an example of how to load dat from the database into the website
-export const load = async ({ locals }: PageServerLoadEvent) => {
+export const load = async (event: PageServerLoadEvent): Promise<{ sessions: any[] } | null> => {
     try {
         // try and get some data from the data base
-        let data = await locals.pb.collection("users").getFullList();
-        return data[0];
+        const data = await event.locals.pb.collection("sessions").getFullList();
+        return { sessions: data };
     } catch (e) {
         // default data if there is some error
-        return { "nodata": "oh no" };
+        return null;
     }
 }
---
+++ src/routes/sessionlist/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let data: { sessions: any[] | null };
+</script>
+
+<h1>Session List</h1>
+
+{#if data.sessions == null}
+	<p>Loading...</p>
+{:else}
+	<ul>
+		{#each data.sessions as session}
+			<li>
+				<p>id: {session.id}</p>
+				<p>state: {session.state}</p>
+				<p><a href={`/joinsession?id=${session.id}`}>link</a></p>
+			</li>
+		{/each}
+	</ul>
+{/if}
