2f7490c4790639b8f9ac761cc46cb4e521a3c799
Daniel Stainer <e52348ds@e-c10kig2319.it.manchester.ac.uk>
2024-02-09 12:33:38 +0000

    Link sends user to session page

--- src/routes/joinsession/+page.svelte
+++ src/routes/joinsession/+page.svelte
@@ -1,7 +1,17 @@
 <script>
 	export let data;
-
+	function createFormLink(form) {
+		let url = "/session";
+		if (form == undefined) {
+			return undefined;
+		}
+		let sps = new URLSearchParams();
+		sps.append("id", form.id);
+		return url + "?" + sps.toString();
+	}
+	$: formLink = createFormLink(data.response);
 </script>
+
 <form method="POST">
 	<label>
 		Session ID
@@ -13,3 +23,11 @@
 	</label>
 	<button>Log in</button>
 </form>
+
+{#if formLink != undefined}
+	<p>User created!</p>
+	<p>User Id: {data.response.id}</p>
+	<p>Link: <a href={formLink}>{formLink}</a> <button on:click={() => {
+		navigator.clipboard.writeText(window.location.host + formLink);
+	}}>Copy Link</button></p>
+{/if}
