519e2ab9eca14c5f0e53317e2d618d8b8c5475b1
Jonathan Wilson <jonathan.wilson-6@student.manchester.ac.uk>
2024-02-09 12:44:53 +0000

    join session page backup

---
+++ src/routes/joinsession/my_+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let data;
+    $: formContents = data.prefill;
+    // function createJoinSessionLink()
+</script>
+
+<form method="POST">
+    <input type="text" bind:value={formContents.sessionId} name="sessionId">
+    <!-- <input type="text" bind:value={formContents.nickname} name="nickname"> -->
+    <input type="text" name="nickname">
+    <button>Join Session</button>
+</form>
+
+{#if false}
+    <p>Link to join session if button didn't work</p>
+    <!-- <a href=""></a> -->
+    <!-- <p>Link: <a href={joinSessionLink}>{joinSessionLink}</a> <button on:click={() => {
+		navigator.clipboard.writeText(window.location.host + joinSessionLink);
+	}}>Copy Link</button></p> -->
+{/if}
