2879de032ee2d064b5627683a9a19ad4e4e29593
Daniel Stainer <e52348ds@e-c10kig2319.it.manchester.ac.uk>
2024-02-09 12:06:49 +0000

    Added form to joinSession

---
+++ src/routes/joinsession/+page.svelte
@@ -0,0 +1,11 @@
+<form method="POST">
+	<label>
+		Session ID
+		<input name="session_id" type="number">
+	</label>
+	<label>
+		Username
+		<input name="username" type="text">
+	</label>
+	<button>Log in</button>
+</form>
