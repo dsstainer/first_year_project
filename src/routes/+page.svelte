ac2e57d48bf28ddb3094c3d91ce3a7edc5ab4a6d
q86084tu <thomas.ubhi@student.manchester.ac.uk>
2024-03-18 17:14:19 +0000

    Home and About page added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,12 @@
 	<div class="page-break"></div>
 	<div class='page-content'>
 		<div class='page-content-explanation'>
-			<p>Some explanation about this game</p>
+			<h1>How to play - </h1> 
+			<p>- First players either join or create a new session using the links in the navigation bar at the top</p>
+			<p>- After four players have joined, the game can begin!</p>
+			<p>- Players must draw the opposite of the prompt displayed on screen </p>
+			<p>- After each player has submitted, users must vote on the best drawing</p>
+			<p>- Finally, the votes are collected and the results are displayed in a leaderboard!</p>
 		</div>
 	</div>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,16 @@
 	<div class="page-break"></div>
 	<div class='page-content'>
 		<div class='page-content-explanation'>
-			<p>Some explanation about this game</p>
+			<h1>About us - </h1>
+			<p>-FlipPad is a 4-player online game designed to pit your friends against one another in a test of artistic ability!</p>
+			<p>-Players are tasked with drawing the opposite of a given prompt, allowing users to reverse the norm and unleash their creativity in their unique interpretation of the word. </p>
+			<h2>What makes FlipPad unique?</h2>
+			<p>-By turning traditional drawing games on their head, FlipPad invites you to think outside the box and showcase your imagination! </p>
+			<p>-From everyday objects to abstract concepts, we offer endless fun and challenge for you and your friends. </p>
+			<h3> Who can play FlipPad? </h3>
+			<p>-FlipPad is accessible to all, being the perfect game for players of all ages with its simple rules and intuitive gameplay!</p>
+
+
 		</div>
 	</div>
 </div>
