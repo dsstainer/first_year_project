64555e5634272af70a43ddf369b87d1875d34978
Jonathan Wilson <jonathan.wilson-6@student.manchester.ac.uk>
2024-04-06 16:23:28 +0100

    added 'already 4 users in session to session list'

--- src/routes/sessionlist/+page.server.ts
+++ src/routes/sessionlist/+page.server.ts
@@ -5,7 +5,13 @@ export const load = async (event: PageServerLoadEvent): Promise<{ sessions: any[
     try {
         // try and get some data from the data base
         const data = await event.locals.pb.collection("sessions").getFullList();
-        data.sessions = addColorsToSession(data);
+        addColorsToSession(data);
+        for (const session of data) {
+            const usersInSession = await event.locals.pb.collection("users").getFullList({
+                filter: `session_id="${session.id}"`
+            });
+            session.numUsers = usersInSession.length;
+        }
         return { sessions: data };
     } catch (e) {
         // default data if there is some error
@@ -14,31 +20,28 @@ export const load = async (event: PageServerLoadEvent): Promise<{ sessions: any[
 }
 
 const colors = ['#FF8D8D', '#FFF7AC', '#AFFFAD', '#DEA8FF', '#8DB4FF'];
-function getBackgroundColor(id){
+function getBackgroundColor(id: string) {
     if (id.charCodeAt(0) < 55) {
         return colors[0];
     }
-    else if(id.charCodeAt(0)< 100){
+    else if (id.charCodeAt(0) < 100) {
         return colors[1];
     }
-    else if(id.charCodeAt(0)<107){
+    else if (id.charCodeAt(0) < 107) {
         return colors[2];
     }
-    else if(id.charCodeAt(0)<114){
+    else if (id.charCodeAt(0) < 114) {
         return colors[3];
     }
-    else{
+    else {
         return colors[4];
     }
 
     return color;
 }
 
-function addColorsToSession(data){
-    let result = [];
-    data.forEach((session)=>{
+function addColorsToSession(data: any) {
+    data.forEach((session: any) => {
         session.color = getBackgroundColor(session.id);
-        result.push(session);
     });
-    return result;
 }
--- src/routes/sessionlist/+page.svelte
+++ src/routes/sessionlist/+page.svelte
@@ -1,37 +1,48 @@
 <script lang="ts">
-    // this will be the value returned from the load function in +page.server.ts
+	// this will be the value returned from the load function in +page.server.ts
 	export let data: { sessions: any[] | null };
+
+	console.log(data);
 </script>
 
-
-
-<div class='page-container'>
-	<div class='page-title'>
+<div class="page-container">
+	<div class="page-title">
 		<p>Choose a session to join</p>
 	</div>
-	<div class='page-break'></div>
+	<div class="page-break"></div>
 	{#if data.sessions == null || data.sessions.length == 0}
-	<div class='page-content'>
-		<p>No sessions available at the moment</p>
-	</div>
-	{:else}
-		<ul class='page-content-list'>
-		{#each data.sessions as session}
-		<div class='page-content' style='background-color:{session.color}'>
-			<li>
-				<h2>Session ID: {session.id}</h2>
-				<p class='no-margin'>Session State: {session.state}</p>
-				<div class='page-content-list-break'></div>
-				<p class='no-margin'>
-				<a href={`/joinsession?sessionId=${session.id}`}>Press me To Join Session!</a>
-				<button on:click={() => {
-						navigator.clipboard.writeText(window.location.host + `/joinsession?sessionId=${session.id}`);}}>
-					Copy Link
-				</button>
-				</p>
-			</li>
+		<div class="page-content">
+			<p>No sessions available at the moment</p>
 		</div>
-		{/each}
+	{:else}
+		<ul class="page-content-list">
+			{#each data.sessions as session}
+				<div class="page-content" style="background-color:{session.color}">
+					<li>
+						<h2>Session ID: {session.id}</h2>
+						<p class="no-margin">Session State: {session.state}</p>
+						<div class="page-content-list-break"></div>
+						<p class="no-margin">
+							{#if session.numUsers <= 3}
+								<a href={`/joinsession?sessionId=${session.id}`}>Press me To Join Session!</a>
+								<button
+									on:click={() => {
+										navigator.clipboard.writeText(
+											window.location.host + `/joinsession?sessionId=${session.id}`
+										);
+									}}
+								>
+									Copy Link
+								</button>
+							{:else}
+								<div class="page-content-content">
+									<p class="error">Error - there are already 4 users in this session</p>
+								</div>
+							{/if}
+						</p>
+					</li>
+				</div>
+			{/each}
 		</ul>
 	{/if}
 </div>
