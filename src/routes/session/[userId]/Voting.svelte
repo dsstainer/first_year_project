972f19d0f0df41f5fd301f56b3a79b3d80ccc516
z07829yb <yuming.bai@student.manchester.ac.uk>
2024-04-10 16:03:11 +0100

    add feedback and some styling

--- src/routes/session/[userId]/Voting.svelte
+++ src/routes/session/[userId]/Voting.svelte
@@ -1,41 +1,48 @@
 <script>
-	export let images;
-	export let socket;
-	export let userId;
-	const submitVote = (id) => () => {
-		socket.emit('vote', id);
-	};
+    export let images;
+    export let socket;
+    export let userId;
+    let voteSuccess = false;
+
+    const submitVote = (id) => () => {
+        socket.emit('vote', id);
+        voteSuccess = true;
+    };
 </script>
 
 <h1>Voting</h1>
-
-<div class="grid-container">
-	{#each images as image_data}
-		<div class="item">
-			<h2>{image_data.userNickname}</h2>
-			<img src={image_data.image} alt="Player 1" />
-			{#if image_data.userId == userId}
-				<p>Can't vote on your own picture!</p>
-			{:else}
-				<button on:click={submitVote(image_data.userId)}>Vote</button>
-			{/if}
-		</div>
-	{/each}
+<div class="page-break"></div>
+<div class="page-container">
+    <div class="grid-container">
+        {#each images as image_data}
+            <div class="item">
+                <h2>{image_data.userNickname}</h2>
+                <img src={image_data.image} alt="Player 1" />
+                {#if image_data.userId == userId}
+                    <p>Can't vote on your own picture!</p>
+                {:else}
+                    <button on:click={submitVote(image_data.userId)}>Vote</button>
+                {/if}
+            </div>
+        {/each}
+    </div>
+    {#if voteSuccess}
+        <p>Vote successfully submitted!</p>
+    {/if}
 </div>
-
 <style>
-	.item {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		background-color: #8db4ff;
-	}
+    .item {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: #8db4ff;
+    }
 
-	.grid-container {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		gap: 40px;
-		padding: 20px;
-		border: 1px solid #59069a;
-	}
+    .grid-container {
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        gap: 40px;
+        padding: 20px;
+        border: 1px solid #59069a;
+    }
 </style>
